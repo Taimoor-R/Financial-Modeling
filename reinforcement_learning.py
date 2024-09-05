53823098d1623b84d3b00fc52718d7a129d64047
Taimoor Rizwan <taimoorriz@hotmail.com>
2024-09-05 19:17:10 +0100

    fixed issue with file loading for relearning

--- reinforcement_learning.py
+++ reinforcement_learning.py
@@ -1,6 +1,7 @@
 import numpy as np
 import gym
 from stable_baselines3 import PPO
+from stable_baselines3.common.vec_env import DummyVecEnv
 
 class StockTradingEnv(gym.Env):
     def __init__(self, data):
@@ -8,11 +9,11 @@ class StockTradingEnv(gym.Env):
         self.data = data
         self.current_step = 0
         self.action_space = gym.spaces.Discrete(3)  # Buy, Hold, Sell
-        self.observation_space = gym.spaces.Box(low=0, high=np.inf, shape=(4,), dtype=np.float32)
+        self.observation_space = gym.spaces.Box(low=0, high=np.inf, shape=(self.data.shape[1],), dtype=np.float32)  # Updated shape
 
     def reset(self):
         self.current_step = 0
-        return self.data.iloc[self.current_step].values
+        return self.data.iloc[self.current_step].values.astype(np.float32)  # Ensure dtype matches observation_space
 
     def step(self, action):
         self.current_step += 1
@@ -21,24 +22,31 @@ class StockTradingEnv(gym.Env):
             self.current_step = 0
         else:
             done = False
-        
-        reward = 0
-        if action == 0:  # Buy
-            reward = self.data.iloc[self.current_step]['Close'] - self.data.iloc[self.current_step-1]['Close']
-        elif action == 2:  # Sell
-            reward = self.data.iloc[self.current_step-1]['Close'] - self.data.iloc[self.current_step]['Close']
-        
-        obs = self.data.iloc[self.current_step].values
+
+        if self.current_step == 0:
+            reward = 0  # No previous close price to compare
+        else:
+            prev_close = self.data.iloc[self.current_step - 1]['Close']
+            current_close = self.data.iloc[self.current_step]['Close']
+
+            if action == 0:  # Buy
+                reward = current_close - prev_close
+            elif action == 2:  # Sell
+                reward = prev_close - current_close
+            else:  # Hold
+                reward = 0
+
+        obs = self.data.iloc[self.current_step].values.astype(np.float32)  # Ensure dtype matches observation_space
         return obs, reward, done, {}
 
     def render(self, mode='human'):
         pass
 
 def train_reinforcement_learning_model(data):
-    env = StockTradingEnv(data)
+    env = DummyVecEnv([lambda: StockTradingEnv(data)])
     model = PPO('MlpPolicy', env, verbose=1)
     model.learn(total_timesteps=10000)
-    model.save("stock_trading_model")
+    model.save("stock_trading_model")  # Save model without extra extension
 
 def load_reinforcement_learning_model(filename):
     model = PPO.load(filename)
